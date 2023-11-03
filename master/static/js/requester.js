21884fcca9c739c84ab2637b537dfc73cf2d3bee
ManGe <2912882908@qq.com>
2023-11-03 11:53:37 +0800

    .

--- master/static/js/requester.js
+++ master/static/js/requester.js
@@ -5,6 +5,7 @@ const app = createApp({
         return {
             mail: Mail,
             webSite: AddWebSite,
+            bodyType: "json",
         }
     },
     created:function(){
@@ -14,6 +15,70 @@ const app = createApp({
     destroyed:function () {
     },
     methods: {
+        closeSet: function () {
+            console.log("closeSet");
+            $("#setHeaderTable").hide();
+            $("#setQueryTable").hide();
+            $("#setBodyTable").hide();
+            $("#openHeaderLi").removeClass("select_activity");
+            $("#openQueryLi").removeClass("select_activity");
+            $("#openBodyLi").removeClass("select_activity");
+        },
+        openHeaderTable: function () {
+            let t =this;
+            console.log("openHeaderTable");
+            t.closeSet();
+            $("#setHeaderTable").show();
+            $("#openHeaderLi").addClass("select_activity");
+        },
+        openQueryTable: function () {
+            let t =this;
+            console.log("openQueryTable");
+            t.closeSet();
+            $("#setQueryTable").show();
+            $("#openQueryLi").addClass("select_activity");
+        },
+        openBodyTable: function () {
+            let t =this;
+            console.log("openBodyTable");
+            t.closeSet();
+            $("#setBodyTable").show();
+            $("#openBodyLi").addClass("select_activity");
+        },
+        closeBodyMain: function () {
+            $("#bodyJson").hide();
+            $("#bodyFromData").hide();
+            $("#bodyXwwwFrom").hide();
+            $("#bodyXml").hide();
+            $("#bodyText").hide();
+        },
+        openBodyMain: function (type) {
+            let t =this;
+            t.bodyType = type;
+            t.closeBodyMain();
+            switch (type) {
+                case "json":
+                    console.log("json");
+                    $("#bodyJson").show();
+                    break;
+                case "from-data":
+                    console.log("from-data");
+                    $("#bodyFromData").show();
+                    break;
+                case "x-www-form-urlencoded":
+                    console.log("x-www-form-urlencoded");
+                    $("#bodyXwwwFrom").show();
+                    break;
+                case "xml":
+                    console.log("xml");
+                    $("#bodyXml").show();
+                    break;
+                case "plain":
+                    console.log("plain");
+                    $("#bodyText").show();
+                    break;
+            }
+        },
     },
     computed: {
     },
